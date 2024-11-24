0095086c61a9c0a3029996f080240a1ed9ed7bf8
Boris Yakimov <boris.yakimov@itgix.com>
2024-11-24 13:40:54 +0200

    enable python lsps

--- nvim-lua/lua/plugins/mason.lua
+++ nvim-lua/lua/plugins/mason.lua
@@ -9,8 +9,8 @@ return {
 			-- TODO: need to make sure this is not causing 100% CPU spikes like it did before
 			-- TODO: test that enabling or disabling these does not impact performance of nvim
 			"terraformls", -- terraform lsp
-			-- "pyright", -- recommended python lsp
-			-- "ruff", -- adds formatting and linting for python since pyright doesn't provide them
+			"pyright", -- recommended python lsp
+			"ruff", -- adds formatting and linting for python since pyright doesn't provide them
 		})
 	end,
 }
