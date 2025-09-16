de34d733c464c398cfa5d8b550e85346342eccca
Boris Yakimov <yakimovb@gmail.com>
2025-09-17 02:32:14 +0300

    add clangd to mason config

--- nvim-lua/lua/plugins/mason.lua
+++ nvim-lua/lua/plugins/mason.lua
@@ -7,11 +7,10 @@ return {
 		-- extend default list of LSPs to ensure they are installed
 		opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
 			"gopls", -- official go lsp
-			-- TODO: need to make sure this is not causing 100% CPU spikes like it did before
-			-- TODO: test that enabling or disabling these does not impact performance of nvim
 			"terraformls", -- terraform lsp
 			"pyright", -- recommended python lsp
 			"ruff", -- adds formatting and linting for python since pyright doesn't provide them
+			"clangd", -- c/c++ lsp
 		})
 	end,
 }
