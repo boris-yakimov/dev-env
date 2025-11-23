9941de50b3cf6eee73e3a00014a6183cc1b8fa4b
Boris Yakimov <yakimovb@gmail.com>
2025-11-23 22:39:37 +0200

    fix steam command in wofi

--- linux/hyprland/wofi/wofi-menu.sh
+++ linux/hyprland/wofi/wofi-menu.sh
@@ -24,6 +24,6 @@ Dolphin)
   exec dolphin
   ;;
 Steam)
-  exec steam
+  exec env GDK_SCALE=1 steam # GDK_SCALE to fix issue where steam is blurry due to the 1.5 scaling of the 4k monitor (it seems it cannot interpred fractional scaling so we need to use 1 for 100%, 2 for 200%, etc)
   ;;
 esac
