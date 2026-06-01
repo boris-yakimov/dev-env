8a264e988ebddee9141d87bffbb6c7372b4962f6
Boris Yakimov <boris.yakimov@itgix.com>
2026-06-01 18:08:36 +0300

    initial prep to refactor hyprland to lua

---
+++ linux/hyprland/configs/monitors.lua
@@ -0,0 +1,31 @@
+-- https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- default setup
+-- hl.monitor({
+-- 	output = "",
+-- 	mode = "preferred",
+-- 	position = "auto",
+-- 	scale = "auto",
+-- })
+
+-- main PC - 4k / 32 inch
+-- hl.monitor({
+-- 	output = "DP-1",
+-- 	mode = "preferred",
+-- 	position = "auto",
+-- 	scale = "1.5",
+-- })
+
+-- work Laptop
+hl.monitor({
+	output = "HDMI-A-1", -- main monitor
+	mode = "2560x1440@59",
+	position = "1280x0",
+	scale = "1.07",
+})
+
+hl.monitor({
+	output = "eDP-1", -- laptop monitor
+	mode = "1920x1200@60",
+	position = "0x0",
+	scale = "1.50",
+})
---
+++ linux/hyprland/configs/vars.lua
@@ -0,0 +1,5 @@
+local terminal = "ghostty"
+local fileManager = "dolphin"
+-- TODO: test switching to hyprlauncher
+local menu = "wofi"
+local notifications = "swaync"
